8a91f6405beb5092d89e60ad5622d539761d99a8
valencastrillon <valencastrillon2@gmail.com>
2026-05-14 17:10:17 -0500

    session 2 variables, loops, functions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,70 @@
+fn main() {
+    let mut count: i32 = 1;
+    loop{
+    println!("{}", count);
+    if count == 5 {
+    break;
+    }
+    count+=1;
+}
+}
 
 
-fn greet(name:&str){
-    println!("Heyyy {}",name);
+
+
+/* Bucles (while) 
+fn main(){
+    let mut count: i32 = 1;
+    while count<=5 {
+        println!("{}",count);
+        count+=1;
+}
+*/
+
+
+
+/* Bucles (para que vaya hasta el 5, colocarle un =5)
+fn main(){
+    for i in 1..5 {
+        println!("Round {}", i);
+    }
+}
+*/
+
+
+
+/* Condicionales
+fn main(){
+    let age: i32 = 18;
+    if age >= 18 {
+        println!("You are an adult.");
+    } else {
+        println!("You are a minor.");
+    }
+}
+*/
+
+
+
+/* Funciones
+fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 fn main(){
-    
+    let result: i32 = add(2, 3);
+    println!("{}", result);
+}
+*/
+
+
+
+/* Saludos 
+fn greet(name: &str) {
+    println!("Heyyy {}", name)
+}
+
+fn main() {
     greet("Valen");
 }
+*/
